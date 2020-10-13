f8784d949c4415c7fd4f6f7e254a557f4f7d85f7
mabryant4 <70730779+mabryant4@users.noreply.github.com>
2020-10-13 12:21:33 -0500

    Some more search functionality

--- app/src/main/java/com/mychef/mychef/SearchableActivity.kt
+++ app/src/main/java/com/mychef/mychef/SearchableActivity.kt
@@ -5,6 +5,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class SearchableActivity : AppCompatActivity() {
 
@@ -21,6 +28,19 @@ class SearchableActivity : AppCompatActivity() {
     }
 
     private fun doMySearch(query: String) {
+        val textView = findViewById<TextView>(R.id.searchText)
 
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://mychef-web.herokuapp.com/api/v1/show_recipes?search=$query"
+
+        val jsonArrayRequest = JsonArrayRequest(Request.Method.GET, url, null,
+            Response.Listener { response ->
+                textView.text = "%s".format(response.toJSONObject(response).toString())
+            },
+            Response.ErrorListener { error ->
+                textView.text = "That didn't work!"
+            }
+        )
+        queue.add(jsonArrayRequest)
     }
 }
