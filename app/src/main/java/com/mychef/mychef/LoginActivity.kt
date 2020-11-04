5cbd6ad8e8d6079d6655ff5d9bac79e1065da066
Andrew Sartor <62850255+atSartor@users.noreply.github.com>
2020-11-04 16:11:20 -0600

    added framework for login handling - having issues with dependencies and android studio having trouble identifying code snippets from Google as valid.

--- app/src/main/java/com/mychef/mychef/LoginActivity.kt
+++ app/src/main/java/com/mychef/mychef/LoginActivity.kt
@@ -1,19 +1,24 @@
 package com.mychef.mychef
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
 
 class LoginActivity : AppCompatActivity() {
+    private var mAuth: FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
         val button = findViewById<Button>(R.id.button_testLogin)
         button.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
     }
+
+
 }
