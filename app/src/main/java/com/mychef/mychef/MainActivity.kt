237feddb17d0e9d98b1128bcc4f4cd49c412691b
Andrew Sartor <62850255+atSartor@users.noreply.github.com>
2020-11-05 14:36:25 -0600

    Firebase login now works, implementing logging out

--- app/src/main/java/com/mychef/mychef/MainActivity.kt
+++ app/src/main/java/com/mychef/mychef/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.mychef.mychef
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
@@ -19,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         val listFragment = ListFragment()
         val moreFragment = MoreFragment()
         val searchFragment = SearchFragment()
+        val loginActivity = LoginActivity()
 
         setCurrentFragment(favoritesFragment)
 
