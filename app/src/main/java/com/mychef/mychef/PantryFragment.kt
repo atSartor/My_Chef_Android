d8dadae32039800caf7cbcbfa72c6471b5e122b2
Andrew Sartor <62850255+atSartor@users.noreply.github.com>
2020-11-09 17:01:16 -0600

    Revert "Merge pull request #2 from kdbaker2/pantry"
    This reverts commit 39b03ca40a4bd66af46e83e43fd23caedddbc0a7, reversing
    changes made to cd6ade7cc47a6849cca4266ef4af8a0f19a2a08c.

--- app/src/main/java/com/mychef/mychef/PantryFragment.kt
+++ app/src/main/java/com/mychef/mychef/PantryFragment.kt
@@ -1,17 +1,7 @@
 package com.mychef.mychef
 
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ListView
 import androidx.fragment.app.Fragment
-import android.os.Bundle
-import android.R
-import android.content.Context
 
-class PantryFragment: Fragment() {
-
+class PantryFragment: Fragment(R.layout.fragment_pantry) {
 
 }
-
-
