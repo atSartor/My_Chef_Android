65015e2f06698a176897ed8f9f347c11fcd6467f
Erik Mortimer <erikmortimermail@gmail.com>
2020-12-01 22:33:05 -0600

    Implemented calendar with temp add recipe by name to calendar

--- app/src/main/java/com/mychef/mychef/ListFragment.kt
+++ app/src/main/java/com/mychef/mychef/ListFragment.kt
@@ -1,7 +1,276 @@
 package com.mychef.mychef
 
+
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.view.inputmethod.InputMethodManager
+import android.widget.FrameLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.view.children
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.kizitonwose.calendarview.model.CalendarDay
+import com.kizitonwose.calendarview.model.CalendarMonth
+import com.kizitonwose.calendarview.model.DayOwner
+import com.kizitonwose.calendarview.ui.DayBinder
+import com.kizitonwose.calendarview.ui.MonthHeaderFooterBinder
+import com.kizitonwose.calendarview.ui.ViewContainer
+import com.mychef.mychef.databinding.ListFragCalendarDayBinding
+import com.mychef.mychef.databinding.ListFragCalendarHeaderBinding
+import com.mychef.mychef.databinding.ListFragRecipeItemViewBinding
+import com.mychef.mychef.databinding.FragmentListBinding
+import java.time.LocalDate
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
+import java.util.*
 
-class ListFragment: Fragment(R.layout.fragment_list) {
+data class Recipe(val id: String, val text: String, val date: LocalDate)
 
+class ListFragmentAdapter(val onClick: (Recipe) -> Unit) :
+    RecyclerView.Adapter<ListFragmentAdapter.ListFragRecipesViewHolder>() {
+
+    val events = mutableListOf<Recipe>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListFragRecipesViewHolder {
+        return ListFragRecipesViewHolder(
+            ListFragRecipeItemViewBinding.inflate(parent.context.layoutInflater, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(viewHolder: ListFragRecipesViewHolder, position: Int) {
+        viewHolder.bind(events[position])
+    }
+
+    override fun getItemCount(): Int = events.size
+
+    inner class ListFragRecipesViewHolder(private val binding: ListFragRecipeItemViewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            itemView.setOnClickListener {
+                onClick(events[bindingAdapterPosition])
+            }
+        }
+
+        fun bind(recipe: Recipe) {
+            binding.itemRecipeText.text = recipe.text
+        }
+    }
+}
+
+class ListFragment : Fragment(R.layout.fragment_list) {
+
+    private val eventsAdapter = ListFragmentAdapter {
+        AlertDialog.Builder(requireContext())
+            .setMessage("Delete this recipe?")
+            .setPositiveButton("Delete") { _, _ ->
+                deleteEvent(it)
+            }
+            .setNegativeButton("Close", null)
+            .show()
+    }
+
+    private val inputDialog by lazy {
+        val editText = AppCompatEditText(requireContext())
+        val layout = FrameLayout(requireContext()).apply {
+            // Setting the padding on the EditText only pads the input area
+            // not the entire EditText so we wrap it in a FrameLayout.
+            val padding = dpToPx(20, requireContext())
+            setPadding(padding, padding, padding, padding)
+            addView(editText, FrameLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT))
+        }
+        AlertDialog.Builder(requireContext())
+            .setTitle("Enter recipe title")
+            .setView(layout)
+            .setPositiveButton("Save") { _, _ ->
+                saveEvent(editText.text.toString())
+                // Prepare EditText for reuse.
+                editText.setText("")
+            }
+            .setNegativeButton("Close", null)
+            .create()
+            .apply {
+                setOnShowListener {
+                    // Show the keyboard
+                    editText.requestFocus()
+                    context.inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+                }
+                setOnDismissListener {
+                    // Hide the keyboard
+                    context.inputMethodManager.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0)
+                }
+            }
+    }
+
+    private var selectedDate: LocalDate? = null
+    private val today = LocalDate.now()
+
+    private val titleSameYearFormatter = DateTimeFormatter.ofPattern("MMMM")
+    private val titleFormatter = DateTimeFormatter.ofPattern("MMM yyyy")
+    private val selectionFormatter = DateTimeFormatter.ofPattern("d MMM yyyy")
+    private val events = mutableMapOf<LocalDate, List<Recipe>>()
+
+    private lateinit var binding: FragmentListBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentListBinding.bind(view)
+        binding.listFragRv.apply {
+            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+            adapter = eventsAdapter
+            addItemDecoration(DividerItemDecoration(requireContext(), RecyclerView.VERTICAL))
+        }
+
+        val daysOfWeek = daysOfWeekFromLocale()
+        val currentMonth = YearMonth.now()
+        binding.listFragCalendar.apply {
+            setup(currentMonth.minusMonths(10), currentMonth.plusMonths(10), daysOfWeek.first())
+            scrollToMonth(currentMonth)
+        }
+
+        if (savedInstanceState == null) {
+            binding.listFragCalendar.post {
+                // Show today's events initially.
+                selectDate(today)
+            }
+        }
+
+        class DayViewContainer(view: View) : ViewContainer(view) {
+            lateinit var day: CalendarDay // Will be set when this container is bound.
+            val binding = ListFragCalendarDayBinding.bind(view)
+
+            init {
+                view.setOnClickListener {
+                    if (day.owner == DayOwner.THIS_MONTH) {
+                        selectDate(day.date)
+                    }
+                }
+            }
+        }
+        binding.listFragCalendar.dayBinder = object : DayBinder<DayViewContainer> {
+            override fun create(view: View) = DayViewContainer(view)
+            override fun bind(container: DayViewContainer, day: CalendarDay) {
+                container.day = day
+                val textView = container.binding.listFragDayText
+                val dotView = container.binding.listFragDotView
+
+                textView.text = day.date.dayOfMonth.toString()
+
+                if (day.owner == DayOwner.THIS_MONTH) {
+                    textView.makeVisible()
+                    when (day.date) {
+                        today -> {
+                            textView.setTextColorRes(R.color.list_frag_white)
+                            textView.setBackgroundResource(R.drawable.list_frag_today_bg)
+                            dotView.makeInVisible()
+                        }
+                        selectedDate -> {
+                            textView.setTextColorRes(R.color.list_frag_blue)
+                            textView.setBackgroundResource(R.drawable.list_frag_selected_bg)
+                            dotView.makeInVisible()
+                        }
+                        else -> {
+                            textView.setTextColorRes(R.color.list_frag_black)
+                            textView.background = null
+                            dotView.isVisible = events[day.date].orEmpty().isNotEmpty()
+                        }
+                    }
+                } else {
+                    textView.makeInVisible()
+                    dotView.makeInVisible()
+                }
+            }
+        }
+
+        binding.listFragCalendar.monthScrollListener = {
+//            homeActivityToolbar.title = if (it.year == today.year) {
+//                titleSameYearFormatter.format(it.yearMonth)
+//            } else {
+//                titleFormatter.format(it.yearMonth)
+//            }
+
+            // Select the first day of the month when
+            // we scroll to a new month.
+            selectDate(it.yearMonth.atDay(1))
+        }
+
+        class MonthViewContainer(view: View) : ViewContainer(view) {
+            val legendLayout = ListFragCalendarHeaderBinding.bind(view).legendLayout.root
+        }
+        binding.listFragCalendar.monthHeaderBinder = object : MonthHeaderFooterBinder<MonthViewContainer> {
+            override fun create(view: View) = MonthViewContainer(view)
+            override fun bind(container: MonthViewContainer, month: CalendarMonth) {
+                // Setup each header day text if we have not done that already.
+                if (container.legendLayout.tag == null) {
+                    container.legendLayout.tag = month.yearMonth
+                    container.legendLayout.children.map { it as TextView }.forEachIndexed { index, tv ->
+                        tv.text = daysOfWeek[index].name.first().toString()
+                        tv.setTextColorRes(R.color.list_frag_black)
+                    }
+                }
+            }
+        }
+
+        binding.listFragAddButton.setOnClickListener {
+            inputDialog.show()
+        }
+    }
+
+    private fun selectDate(date: LocalDate) {
+        if (selectedDate != date) {
+            val oldDate = selectedDate
+            selectedDate = date
+            oldDate?.let { binding.listFragCalendar.notifyDateChanged(it) }
+            binding.listFragCalendar.notifyDateChanged(date)
+            updateAdapterForDate(date)
+        }
+    }
+
+    private fun saveEvent(text: String) {
+        if (text.isBlank()) {
+            Toast.makeText(requireContext(), "No Text", Toast.LENGTH_LONG).show()
+        } else {
+            selectedDate?.let {
+                events[it] = events[it].orEmpty().plus(Recipe(UUID.randomUUID().toString(), text, it))
+                updateAdapterForDate(it)
+            }
+        }
+    }
+
+    private fun deleteEvent(recipe: Recipe) {
+        val date = recipe.date
+        events[date] = events[date].orEmpty().minus(recipe)
+        updateAdapterForDate(date)
+    }
+
+    private fun updateAdapterForDate(date: LocalDate) {
+        eventsAdapter.apply {
+            events.clear()
+            events.addAll(this@ListFragment.events[date].orEmpty())
+            notifyDataSetChanged()
+        }
+        binding.listFragSelectedDateText.text = selectionFormatter.format(date)
+    }
+
+//    override fun onStart() {
+//        super.onStart()
+//        homeActivityToolbar.setBackgroundColor(requireContext().getColorCompat(R.color.example_3_toolbar_color))
+//        requireActivity().window.statusBarColor = requireContext().getColorCompat(R.color.example_3_statusbar_color)
+//    }
+//
+//    override fun onStop() {
+//        super.onStop()
+//        homeActivityToolbar.setBackgroundColor(requireContext().getColorCompat(R.color.colorPrimary))
+//        requireActivity().window.statusBarColor = requireContext().getColorCompat(R.color.colorPrimaryDark)
+//    }
 }
